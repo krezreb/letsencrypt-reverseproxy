dc214be9955c5331673d0da54a94fbfddcc07a3c
Joseph Beeson <josephbeeson@gmail.com>
2021-08-12 14:02:09 +0200

    add basic auth support

--- reverse_proxy.conf.tpl
+++ reverse_proxy.conf.tpl
@@ -3,8 +3,6 @@ server {
 	listen 443 ssl http2 default_server;
 	listen [::]:443 ssl http2 default_server;
 	
-	server_name _;
-
     ssl_certificate         $CERT_PATH;
     ssl_certificate_key     $CERT_KEY_PATH;
 
@@ -16,17 +14,21 @@ server {
 
     listen 80 ;
     server_name _;
-
+    
     client_body_buffer_size  10K;
     client_header_buffer_size 10k;
     client_max_body_size 10k;
     large_client_header_buffers 2 10k;
     
     location / {
+        $AUTH_BASIC
+        $AUTH_BASIC_USER_FILE
+    
         proxy_ssl_trusted_certificate $PROXY_SSL_TRUSTED_CERTIFICATE;
         proxy_ssl_verify $PROXY_SSL_VERIFY;
         client_max_body_size 0;
 
+        proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Host $http_host;
         proxy_pass $PROXY_PASS_TARGET;
--- run.sh
+++ run.sh
@@ -5,20 +5,38 @@ trap "kill -9 0" EXIT
 
 set -ue
 
-echo ACME_CERT_PORT is $ACME_CERT_PORT
 echo SETUP_REFRESH_FREQUENCY is $SETUP_REFRESH_FREQUENCY
 echo PROXY_PASS_TARGET is $PROXY_PASS_TARGET
 
-setupssl --port 80
 
-# regularly check if ssl cert needs to be renewed
-(while true ; do sleep $SETUP_REFRESH_FREQUENCY ; setupssl --port $ACME_CERT_PORT; nginx -s reload ;  done) &
+if [[ $SETUP_REFRESH_FREQUENCY > 0 ]] ; then
+    echo ACME_CERT_PORT is $ACME_CERT_PORT
+    setupssl --port 80
+    # regularly check if ssl cert needs to be renewed
+    (while true ; do sleep $SETUP_REFRESH_FREQUENCY ; setupssl --port $ACME_CERT_PORT; nginx -s reload ;  done) &
+else
+    # set SETUP_REFRESH_FREQUENCY to zero if another container does the renewing
+    (while true ; do sleep 86000 ; nginx -s reload ;  done) &
+fi
 
 # build a list of all env var keys
 envkeys=""
 for l in $(printenv | cut -d"=" -f1); do 
     envkeys="$envkeys \\\$$l"
 done
+
+# support for basic auth
+if [[ ${AUTH_BASIC_USER_FILE:=""} != "" ]] ;  then
+    export AUTH_BASIC_USER_FILE="auth_basic_user_file \"$AUTH_BASIC_USER_FILE\";"
+    if [[ ${AUTH_BASIC:=""} != "" ]] ;  then
+        export AUTH_BASIC="auth_basic \"$AUTH_BASIC\";"
+    else
+        export AUTH_BASIC="auth_basic \"HTTP Authentication Required\";"
+    fi
+else
+    export AUTH_BASIC=""
+    export AUTH_BASIC_USER_FILE=""
+fi
 
 # apply env vars to template conf files
 for f in $(find  /etc/nginx/ -type f | grep .tpl$ ); do
